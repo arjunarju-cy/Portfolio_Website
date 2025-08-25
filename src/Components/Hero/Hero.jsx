6489bde872726957851f18f017211e8e639e4828
Arjun <arjuni5acer12@gmail.com>
2025-08-25 10:16:07 +0530

    first commit

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Hero.css'
+import profile_img from '../../assets/profile_img.jpg'
+import AnchorLink from 'react-anchor-link-smooth-scroll'
+
+const Hero = () => {
+  return (
+    <div id='home' className='hero'>
+      <img src={profile_img} alt="" className='profile-img' />
+      <h1><span>I'm Arjun,</span> Full Stack developer based in Kerala.</h1>
+      <p>I am a full stack developer from Calicut, with 1 year of experience in Corpus Data Hub</p>
+      <div className="hero-action">
+        <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect with me</AnchorLink></div>
+        <div className="hero-resume">My resume</div>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
